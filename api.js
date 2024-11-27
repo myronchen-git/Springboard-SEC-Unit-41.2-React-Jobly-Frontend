20ac3bfada687ed9d7ad993f41dff39f52658bb4
Myron Chen <125855279+myronchen-git@users.noreply.github.com>
2024-11-26 18:02:09 -0800

    step 2: complete
    Created API helper.  This includes the functions that the frontend will
    use to call the backend.
    
    Since Vite is used for the frontend, Node's process module will be
    undefined when running the frontend.  As a result, to have the BASE_URL
    variable be correctly assigned for all circumstances, there has to be
    checks for whether process or import.meta is available.
    
    Also updated Axios import path.

--- api.js
+++ api.js
@@ -1,8 +1,15 @@
-import axios from 'axios';
+import axios from './frontend/node_modules/axios';
 
 // ==================================================
 
-const BASE_URL = process.env.REACT_APP_BASE_URL || 'http://localhost:3001';
+let BASE_URL;
+if (typeof process !== 'undefined') {
+  BASE_URL = process.env.REACT_APP_BASE_URL;
+} else if (typeof import.meta !== 'undefined') {
+  BASE_URL = import.meta.env.REACT_APP_BASE_URL;
+}
+
+BASE_URL ||= 'http://localhost:3001';
 
 // --------------------------------------------------
 
@@ -38,16 +45,121 @@ class JoblyApi {
     }
   }
 
+  // --------------------------------------------------
   // Individual API routes
 
-  /** Get details on a company by handle. */
+  /**
+   * Registers a new user.
+   *
+   * @param {Object} user - { username, password, firstName, lastName, email }
+   * @returns {String} A new authentication token.
+   */
+  static async registerUser(user) {
+    const res = await this.request('auth/register', user, 'post');
+    return res.token;
+  }
 
+  /**
+   * Logs in a user.
+   *
+   * @param {Object} userAndPass - { username, password }
+   * @returns {String} A new authentication token.
+   */
+  static async loginUser(userAndPass) {
+    const res = await this.request('auth/token', userAndPass, 'post');
+    return res.token;
+  }
+
+  /**
+   * Gets all companies.
+   *
+   * @param {Object} filters - Query parameters for filtering list of companies.
+   *   Currently, they are name, minEmployees, and maxEmployees.
+   * @returns {Array}
+   * [ { handle, name, description, numEmployees, logoUrl }, ...].
+   */
+  static async getCompanies(filters = {}) {
+    const res = await this.request('companies', filters);
+    return res.companies;
+  }
+
+  /**
+   * Get details on a company by handle.
+   *
+   * @param {String} handle - The handle of the company to get data for.
+   * @returns {Object}
+   * { handle, name, description, numEmployees, logoUrl, jobs }
+   * where jobs is [ { id, title, salary, equity }, ... ].
+   */
   static async getCompany(handle) {
-    let res = await this.request(`companies/${handle}`);
+    const res = await this.request(`companies/${handle}`);
     return res.company;
   }
 
-  // obviously, you'll add a lot here ...
+  /**
+   * Gets all jobs.
+   *
+   * @returns {Array}
+   * [ { id, title, salary, equity, companyHandle, companyName }, ...].
+   */
+  static async getJobs() {
+    const res = await this.request(`jobs`);
+    return res.jobs;
+  }
+
+  /**
+   * Gets data about a job.
+   *
+   * @param {String} id - ID of job to get.
+   * @returns {Object} { id, title, salary, equity, company }
+   * where company is { handle, name, description, numEmployees, logoUrl }.
+   */
+  static async getJob(id) {
+    const res = await this.request(`jobs/${id}`);
+    return res.job;
+  }
+
+  /**
+   * Gets a user's info and job IDs of submitted applications.
+   *
+   * @param {String} username - The username of the account to get.
+   * @returns {Object}
+   * { username, first_name, last_name, is_admin, jobs }
+   * where jobs is [ id, ... ].
+   */
+  static async getUser(username) {
+    const res = await this.request(`users/${username}`);
+    return res.user;
+  }
+
+  /**
+   * Updates a user's info.
+   *
+   * @param {String} username - The username of the user to update.
+   * @param {Object} data - Can include any of
+   * { firstName, lastName, password, email }.
+   * @returns {Object} { username, firstName, lastName, email, isAdmin }
+   */
+  static async patchUser(username, data) {
+    const res = await this.request(`users/${username}`, data, 'patch');
+    return res.user;
+  }
+
+  /**
+   * Has a user apply to a job opening.
+   *
+   * @param {String} username - Username of user applying to a job.
+   * @param {String} jobId - ID of the job being applied to.
+   * @returns {String} The job ID.
+   */
+  static async postApplication(username, jobId) {
+    const res = await this.request(
+      `users/${username}/jobs/${jobId}`,
+      {},
+      'post'
+    );
+    return res.applied;
+  }
 }
 
 // ==================================================
@@ -57,3 +169,7 @@ JoblyApi.token =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZ' +
   'SI6InRlc3R1c2VyIiwiaXNBZG1pbiI6ZmFsc2UsImlhdCI6MTU5ODE1OTI1OX0.' +
   'FtrMwBQwe6Ue-glIFgz_Nf8XxRT2YecFCiSpYL0fCXc';
+
+// ==================================================
+
+export default JoblyApi;
