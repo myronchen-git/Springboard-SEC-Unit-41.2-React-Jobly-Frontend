b520b72031c2926f77012b4ca1c7d88f19569469
Myron Chen <125855279+myronchen-git@users.noreply.github.com>
2024-11-28 13:26:37 -0800

    step 4: feat: create CompaniesPage
    This also completes all of step 4.

--- frontend/src/routes/CompaniesPage.jsx
+++ frontend/src/routes/CompaniesPage.jsx
@@ -1,5 +1,37 @@
+import { useEffect, useState } from 'react';
+
+import JoblyApi from '../../../api';
+import CompanyCard from '../components/CompanyCard';
+import SearchBar from '../components/SearchBar';
+
+// ==================================================
+
+/**
+ * Lists all companies.  Allows the list to be filtered.
+ */
 function CompaniesPage() {
-  return <section>Companies Page</section>;
+  const [companies, setCompanies] = useState([]);
+
+  useEffect(() => {
+    retrieveCompanies();
+  }, []);
+
+  async function retrieveCompanies(filters = {}) {
+    setCompanies(await JoblyApi.getCompanies(filters));
+  }
+
+  return (
+    <main className="CompaniesPage">
+      <SearchBar retrieveItems={retrieveCompanies} filterName="name" />
+      <ul>
+        {companies.map((company) => (
+          <li key={company.handle}>
+            <CompanyCard company={company} />
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
 }
 
 // ==================================================
---
+++ frontend/src/routes/CompaniesPage.test.jsx
@@ -0,0 +1,116 @@
+import { act, fireEvent, render } from '@testing-library/react';
+import { useNavigate } from 'react-router-dom';
+import { beforeEach, describe, expect, it, vi } from 'vitest';
+
+import JoblyApi from '../../../api';
+import { companies } from '../_testCommon.js';
+import CompaniesPage from './CompaniesPage.jsx';
+
+// ==================================================
+
+vi.mock(import('../../../api'), () => {
+  const MockJoblyApi = vi.fn();
+  MockJoblyApi.getCompanies = vi.fn();
+
+  return {
+    default: MockJoblyApi,
+  };
+});
+
+vi.mock('react-router-dom');
+
+// ==================================================
+
+describe('CompaniesPage', () => {
+  const mockNavigate = vi.fn();
+  useNavigate.mockReturnValue(mockNavigate);
+
+  beforeEach(() => {
+    JoblyApi.getCompanies.mockImplementation((filters) => {
+      let companiesToReturn;
+
+      if (Object.keys(filters).length) {
+        // If there are filters.
+
+        const companyName = filters.name;
+
+        companiesToReturn = companies.reduce((arr, company) => {
+          if (company.name.toLowerCase().includes(companyName.toLowerCase())) {
+            arr.push({ ...company });
+          }
+
+          return arr;
+        }, []);
+      } else {
+        // If there are no filters.
+
+        companiesToReturn = structuredClone(companies);
+      }
+
+      return Promise.resolve(companiesToReturn);
+    });
+
+    mockNavigate.mockReset();
+  });
+
+  it('renders.', async () => {
+    await act(async () => render(<CompaniesPage />));
+  });
+
+  it('matches snapshot.', async () => {
+    const { asFragment } = await act(async () => render(<CompaniesPage />));
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it('displays companies and search bar.', async () => {
+    // Act
+    const { getByPlaceholderText, getByText } = await act(async () =>
+      render(<CompaniesPage />)
+    );
+
+    // Assert
+    companies.forEach((company) => {
+      expect(getByText(company.name)).toBeVisible();
+    });
+    expect(getByPlaceholderText('Enter search term...')).toBeVisible();
+  });
+
+  it('filters the list of companies.', async () => {
+    // Arrange
+    const { getByPlaceholderText, getByText, queryByText } = await act(
+      async () => render(<CompaniesPage />)
+    );
+
+    const searchInput = getByPlaceholderText('Enter search term...');
+    const submitBtn = getByText('Submit');
+
+    // Act
+    fireEvent.change(searchInput, { target: { value: companies[0].name } });
+    await act(async () => fireEvent.click(submitBtn));
+
+    // Assert
+    expect(getByText(companies[0].description)).toBeVisible();
+
+    companies.forEach((company, i) => {
+      if (i !== 0) {
+        expect(queryByText(company.name)).not.toBeInTheDocument();
+      }
+    });
+  });
+
+  it("navigates to a specific company's page.", async () => {
+    // Arrange
+    const { getByText } = await act(async () => render(<CompaniesPage />));
+
+    const companyToVisit = getByText(companies[0].name);
+
+    // Act
+    fireEvent.click(companyToVisit);
+
+    // Assert
+    expect(mockNavigate).toHaveBeenCalledOnce();
+    expect(mockNavigate).toHaveBeenCalledWith(
+      `/companies/${companies[0].handle}`
+    );
+  });
+});
