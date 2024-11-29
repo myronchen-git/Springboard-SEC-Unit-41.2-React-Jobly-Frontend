9555699814c9c8118a11de5854cac3c200e24eca
Myron Chen <125855279+myronchen-git@users.noreply.github.com>
2024-11-29 11:50:38 -0800

    step 6: feat: update HomePage
    Updated HomePage to display different screens for whether a user is
    logged in or not.  There was no need to have isLoggedIn as a prop if
    username can determine that.
    
    Also added in tests.

--- frontend/src/routes/HomePage.jsx
+++ frontend/src/routes/HomePage.jsx
@@ -1,7 +1,43 @@
-function HomePage() {
-  return <section>Home Page</section>;
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+// ==================================================
+
+/**
+ * Displays the home page.  Displays different views depending on whether a
+ * user is logged in or not.
+ *
+ * @param {Object} props - React component properties.
+ * @param {String} props.username - Username of the logged in user, otherwise
+ *   falsy.
+ */
+function HomePage({ username }) {
+  return (
+    <main>
+      <h1>Jobly</h1>
+      <p>All the jobs in one, convenient place.</p>
+      {username ? (
+        <div>
+          <p>Welcome Back, {username}!</p>
+        </div>
+      ) : (
+        <div>
+          <Link to="/login">
+            <button type="button">Log In</button>
+          </Link>
+          <Link to="/signup">
+            <button type="button">Sign Up</button>
+          </Link>
+        </div>
+      )}
+    </main>
+  );
 }
 
 // ==================================================
+
+HomePage.propTypes = {
+  username: PropTypes.string,
+};
 
 export default HomePage;
---
+++ frontend/src/routes/HomePage.test.jsx
@@ -0,0 +1,60 @@
+import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import { describe, expect, it } from 'vitest';
+
+import HomePage from './HomePage';
+
+// ==================================================
+
+describe('CompanyPage', () => {
+  const username = 'testuser';
+
+  it('renders.', () => {
+    render(
+      <MemoryRouter>
+        <HomePage username={username} />
+      </MemoryRouter>
+    );
+  });
+
+  it('matches snapshot.', () => {
+    const { asFragment } = render(
+      <MemoryRouter>
+        <HomePage username={username} />
+      </MemoryRouter>
+    );
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it('displays correct text when logged in.', () => {
+    // Act
+    const { queryByText, getByText } = render(
+      <MemoryRouter>
+        <HomePage username={username} />
+      </MemoryRouter>
+    );
+
+    // Assert
+    expect(getByText('Welcome Back', { exact: false })).toBeVisible();
+    expect(queryByText('Log In')).not.toBeInTheDocument();
+    expect(queryByText('Sign Up')).not.toBeInTheDocument();
+  });
+
+  it('displays correct text when not logged in.', () => {
+    const username = undefined;
+
+    // Act
+    const { queryByText, getByText } = render(
+      <MemoryRouter>
+        <HomePage username={username} />
+      </MemoryRouter>
+    );
+
+    // Assert
+    expect(
+      queryByText('Welcome Back', { exact: false })
+    ).not.toBeInTheDocument();
+    expect(getByText('Log In')).toBeVisible();
+    expect(getByText('Sign Up')).toBeVisible();
+  });
+});
