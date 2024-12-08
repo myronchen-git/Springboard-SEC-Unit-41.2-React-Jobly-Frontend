d70fe634bc128ed8eeaa1f641027201b860e2ff2
Myron Chen <125855279+myronchen-git@users.noreply.github.com>
2024-12-07 22:01:46 -0800

    fix: change backend URL variable
    Vite requires environment variables to start with "VITE_" in order for
    it to be exposed in the browser.

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from '../node_modules/axios';
 
 // ==================================================
 
-const BASE_URL = import.meta.env.REACT_APP_BASE_URL || 'http://localhost:3001';
+const BASE_URL = import.meta.env.VITE_BACKEND_URL || 'http://localhost:3001';
 
 // --------------------------------------------------
 
